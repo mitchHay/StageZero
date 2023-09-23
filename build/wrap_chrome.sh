80099f83d6d125e5a540abd2437913155a946a8e
mitchHay <mitchell.hayward97@gmail.com>
2023-09-23 15:14:50 +1000

    Fix up UI test step

---
+++ build/wrap_chrome.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Adapted from https://github.com/SeleniumHQ/docker-selenium/blob/trunk/NodeChrome/wrap_chrome_binary
+
+CHROME_PATH=$(which chrome)
+WRAPPER_PATH=$(readlink -f $CHROME_PATH)
+BASE_PATH="$WRAPPER_PATH-base"
+mv "$WRAPPER_PATH" "$BASE_PATH"
+
+cat > "$WRAPPER_PATH" <<_EOF
+#!/bin/bash
+
+# umask 002 ensures default permissions of files are 664 (rw-rw-r--) and directories are 775 (rwxrwxr-x).
+umask 002
+
+# Debian/Ubuntu seems to not respect --lang, it instead needs to be a LANGUAGE environment var
+# See: https://stackoverflow.com/a/41893197/359999
+for var in "\$@"; do
+   if [[ \$var == --lang=* ]]; then
+      LANGUAGE=\${var//--lang=}
+   fi
+done
+
+# Set language environment variable
+export LANGUAGE="\$LANGUAGE"
+
+# Note: exec -a below is a bashism.
+exec -a "\$0" "$BASE_PATH" --no-sandbox "\$@"
+_EOF
+chmod +x "$WRAPPER_PATH"
