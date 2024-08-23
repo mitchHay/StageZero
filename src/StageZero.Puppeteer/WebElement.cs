44fcf86474279ba9254391329fd627656dd74d57
Mitch <mitchell.hayward97@gmail.com>
2024-08-23 23:34:39 +1000

    Added Puppeteer WebElement

---
+++ src/StageZero.Puppeteer/WebElement.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading.Tasks;
+using PuppeteerSharp;
+using PuppeteerSharp.Input;
+using StageZero.Web;
+
+namespace StageZero.Puppeteer;
+
+public class WebElement(IElementHandle elementHandle) : IElementWeb
+{
+    /// <inheritdoc />
+    public string Text => elementHandle.GetPropertyAsync("innerText").GetAwaiter().GetResult()
+        .JsonValueAsync<string>().GetAwaiter().GetResult();
+
+    /// <inheritdoc />
+    public bool IsDisplayed => elementHandle.IsVisibleAsync().GetAwaiter().GetResult();
+
+    /// <inheritdoc />
+    public string ClassName => elementHandle.GetPropertyAsync("class").GetAwaiter().GetResult()
+        .JsonValueAsync<string>().GetAwaiter().GetResult();
+
+    /// <inheritdoc />
+    public string Id => elementHandle.GetPropertyAsync("id").GetAwaiter().GetResult()
+        .JsonValueAsync<string>().GetAwaiter().GetResult();
+
+    /// <inheritdoc />
+    public string Tag => elementHandle.GetPropertyAsync("tag").GetAwaiter().GetResult()
+        .JsonValueAsync<string>().GetAwaiter().GetResult();
+    
+    /// <inheritdoc />
+    public Task PressKeys(Keys keys)
+    {
+        throw new NotImplementedException();
+    }
+    
+    /// <inheritdoc />
+    public Task Type(string text) => elementHandle.TypeAsync(text);
+
+    /// <inheritdoc />
+    public Task Click() => elementHandle.ClickAsync();
+
+    /// <inheritdoc />
+    public Task RightClick() => elementHandle.ClickAsync(new ClickOptions
+    {
+        Button = MouseButton.Right,
+    });
+
+    /// <inheritdoc />
+    public Task DoubleClick() => elementHandle.ClickAsync(new ClickOptions
+    {
+        Count = 2,
+    });
+
+
+    /// <inheritdoc />
+    public Task ClickAndHold(TimeSpan duration) => elementHandle.ClickAsync(new ClickOptions
+    {
+        Delay = duration.Milliseconds,
+    });
+
+    /// <inheritdoc />
+    public Task<string> GetAttributeValue(string attributeName) =>
+        elementHandle.EvaluateFunctionAsync<string>($"node => node.{attributeName}");
+    
+    /// <inheritdoc />
+    public Task<IElementWeb> ScrollTo(string cssSelector)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task SelectOption(string optionText) => elementHandle.SelectAsync(optionText);
+
+    
+    /// <inheritdoc />
+    public Task SelectOption(int optionIndex)
+    {
+        throw new NotImplementedException();
+    }
+}
